62ec5ddbd5a4b3da00a5f1f408bc9a404d3d71a8
Tim Liu <timl@campaignmonitor.com>
2019-08-14 09:28:18 +1000

    Clean up

--- forms/views/public/recaptcha.php
+++
@@ -1,6 +0,0 @@
-
-
-<div class="g-recaptcha" data-sitekey="<?php echo $this->getSitePublic(); ?>"></div>
-<script type="text/javascript"
-        src="https://www.google.com/recaptcha/api.js?hl=<?php echo $this->getLang(); ?>">
-</script>
