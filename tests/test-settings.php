d7104a5d2b79f5ac4dee5a67c84162159afd927e
Tim Liu <timl@campaignmonitor.com>
2019-09-02 16:18:05 +1000

    Revert "clean up"
    This reverts commit b3398552a97b53befc106876f0d721b7a176860a.

---
+++ tests/test-settings.php
@@ -0,0 +1,35 @@
+<?php
+
+use forms\core\Options;
+use forms\core\Settings;
+
+class TestSettings extends WP_UnitTestCase {
+
+	public function test_ClearAndReAddSettings_ShouldSetValidArrayItem() {
+		// Arrange.
+		Settings::clear();
+
+		// Act.	
+		Settings::add( 'client_secret', 'test_secret' );      
+		$cm_settings = Settings::get();
+		echo "final array: \n";
+		print_r($cm_settings);
+		// Assert.
+		$this->assertTrue( is_array( $cm_settings ) );
+		$this->assertSame( $cm_settings['client_secret'], 'test_secret' );
+	}
+
+	public function test_InvalidSettingsAndAddNewSettings_ShouldReSetEmptyArrayAndAddItem() {
+		// Arrange.
+		Options::update( Settings::NAME, 'InvalidString' );
+
+		// Act.
+		Settings::add( 'client_secret', 'test_secret' );      
+		$cm_settings = Settings::get();
+		echo "final array: \n";
+		print_r($cm_settings);
+		// Assert.
+		$this->assertTrue( is_array( $cm_settings ) );
+		$this->assertSame( $cm_settings['client_secret'], 'test_secret' );
+	}
+}
